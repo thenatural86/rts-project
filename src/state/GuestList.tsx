b940093645e6cc6221bedd9c70babcd679f2e084
thenatural86 <philip.smalls86@gmail.com>
2021-02-28 15:16:46 -0500

    set array of string as type for guest piece of state

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 
 const GuestList: React.FC = () => {
   const [name, setName] = useState('')
+  const [guest, setGuest] = useState<string[]>([])
+
+  const onClick = () => {
+    setName('')
+    setGuest([...guest, name])
+  }
+
   return (
     <div>
       <h3>Guest List</h3>
@@ -10,7 +17,7 @@ const GuestList: React.FC = () => {
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
-      <button>Add guest</button>
+      <button onClick={onClick}>Add guest</button>
     </div>
   )
 }
